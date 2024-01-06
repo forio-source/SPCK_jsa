9f876b2e5d09b3b504d382a51cbf7a2a1f086bc2
Namdev23 <nguyennamtiger@gmail.com>
2024-01-06 12:32:16 +0700

    Last update

--- Src/JS/CovaTravel/index.js
+++
@@ -1,7 +0,0 @@
-const account = JSON.parse(localStorage.getItem("Account"));
-
-if (account != undefined) {
-    if (account.usingPack == "Premium") {
-        document.getElementById("ads").remove();
-    };
-};
